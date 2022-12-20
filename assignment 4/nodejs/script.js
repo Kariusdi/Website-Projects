ab3c9cceb651e05fc343060a33a72c3c16f1491e
1909802728861 <cshuhand@gmail.com>
2022-12-20 23:24:37 +0700

    Add error 404 and all webs

---
+++ assignment 4/nodejs/script.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/html/home.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/html/about.html')
+})
+
+app.get('/news', (req, res) => {
+    res.sendFile(__dirname + '/html/news.html')
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/html/error.html')
+})
+
+app.listen(3000, () => {
+    console.log("Start Server at port [3000]")
+})
